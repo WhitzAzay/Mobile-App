d7666dcaf6271807cf2dd81bf12350fd5b1dfee4
WhitzAzay <90360269+WhitzAzay@users.noreply.github.com>
2021-11-17 22:51:27 +0000

    Finished files

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import {FinalList} from "./screens/listItems";
+
 
 export default function App() {
+ const MainNavigator = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <NavigationContainer>
+      <MainNavigator.Navigator screenOptions = {{headerShown : false}}>
+        
+        <MainNavigator.Screen name = "Login" component={Login}/>   
+        <MainNavigator.Screen name = "FinalList" component={FinalList}/>   
+        <MainNavigator.Screen name = "Home" component={Home}/>   
+
+      </MainNavigator.Navigator>
+      </NavigationContainer>
     </View>
   );
-}
+}   
+  
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
